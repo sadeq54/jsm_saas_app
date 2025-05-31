006cef0d5881fd7ff6675ca8811cd3b002ad1f34
Sadeq Majed Al-Sayed Ahmad <135156005+sadeq54@users.noreply.github.com>
2025-05-31 16:33:38 +0300

    remove all the errors amd the app now is connected to the supabase db and authinticated by clerck

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,25 +1,34 @@
-import Image from 'next/image'
-import Link from 'next/link'
-import React from 'react'
-import NavItems from './NavItems'
+import Link from "next/link";
+import Image from "next/image";
+import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+import NavItems from "@/components/NavItems";
 
-export default function Navbar() {
+const Navbar = () => {
     return (
-        <nav className='navbar'>
-            <Link href='/'>
-                <div className='flex items-center gap-2.5 cursor-pointer'>
+        <nav className="navbar">
+            <Link href="/">
+                <div className="flex items-center gap-2.5 cursor-pointer">
                     <Image
-                        src='/images/logo.svg'
-                        alt='Logo'
+                        src="/images/logo.svg"
+                        alt="logo"
                         width={46}
                         height={44}
                     />
                 </div>
             </Link>
-            <div className='flex items-center gap-8'>
+            <div className="flex items-center gap-8">
                 <NavItems />
-                <p>Sign In</p>
+                <SignedOut>
+                    <SignInButton>
+                        <button className="btn-signin">Sign In</button>
+                    </SignInButton>
+                </SignedOut>
+                <SignedIn>
+                    <UserButton />
+                </SignedIn>
             </div>
         </nav>
     )
 }
+
+export default Navbar
